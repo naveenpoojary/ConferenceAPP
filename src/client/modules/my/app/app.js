c84a6c6b0feeb5dbe7826932e78539950d7025fb
naveenpoojary <naveenaug.23@gmail.com>
2019-12-18 12:06:03 +0530

    first commit

---
+++ src/client/modules/my/app/app.js
@@ -0,0 +1,29 @@
+import { LightningElement, track} from 'lwc';
+
+export default class App extends LightningElement {
+    @track sessionId;
+    handleNavigate(event) {
+        this.sessionId = event.detail;
+        this.state = 'details';
+        window.history.pushState('details', null);
+      }
+      @track state;
+      constructor() {
+        super();
+        this.state = 'list';
+        window.history.replaceState('list', null, '');
+        window.onpopstate = event => {
+          if (event.state) {
+            this.state = event.state;
+          }
+        };
+      }
+      get isStateList() {
+        return this.state === 'list';
+      }
+      get isStateDetails() {
+        return this.state === 'details';
+      }
+    }
+
+    
